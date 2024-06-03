2ec8e6321357b55c5014db55eb13be76d84ba476
Michael Naguib <drmichaelnaguib.mn@gmail.com>
2024-06-03 22:09:43 +0300

    updated the metadata

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,11 @@ import InstructorsSection from "@/components/pages/home-page/InstructorsSection"
 import SliderSection from "@/components/pages/home-page/SliderSection";
 import WhoWeAreSection from "@/components/pages/home-page/WhoWeAreSection";
 
+export const metadata = {
+  title: "HomePage",
+  description: "Edmax is an Awsome Website",
+};
+
 export default function Home() {
   return (
     <main>
